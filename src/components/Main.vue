9af4014137fe812d96ae12201b51f854796ae53e
Ryoto Saito <ryoto.no.address@gmail.com>
2019-09-05 19:59:06 +0900

    expand heatmap image and use only 2F image

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <h1>Heatmaps</h1>
-    <img id="heatmap" v-bind:src="imageSrc[currentImg]" />
+    <canvas id="heatmap" width="897px" height="644px" />
+    <img id="img" width="100%" v-bind:src="imageSrc[currentImg]" style="display:none;" v-on:load="imgRender()"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   mounted () {
+    this.ctx = document.getElementById('heatmap').getContext('2d')
     this.imgReload(this)
+    this.imgRender()
     setInterval(this.imgReload, 60 * 1000, this)
     setInterval(this.displayRotation, 10 * 1000, this)
   },
@@ -20,7 +22,7 @@ export default {
   data () {
     return {
       imageSrc: [
-        `http://${window.location.hostname}:3000/floor1/latest.png?`,
+        // `http://${window.location.hostname}:3000/floor1/latest.png?`,
         `http://${window.location.hostname}:3000/floor2/latest.png?`
       ],
       currentImg: 0
@@ -31,6 +33,10 @@ export default {
       vm.imageSrc = vm.imageSrc.map(
         str => `${str.split('?')[0]}?${Date.now().toString()}`
       )
+    },
+    imgRender: () => {
+      let ctx = document.getElementById('heatmap').getContext('2d')
+      ctx.drawImage(document.getElementById('img'), 150, 150, 780, 560, 0, 0, 897, 644)
     },
     displayRotation: (vm) => {
       vm.currentImg = (vm.currentImg + 1) % vm.imageSrc.length
